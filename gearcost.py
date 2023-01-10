9dbf6848162a3ea252b089eeec562e4e65c2c7c6
Calvin <morettc@gmail.com>
2023-01-10 01:19:52 -0500

    Updating / Commenting
    Updating / Commenting

--- gearcost.py
+++ gearcost.py
@@ -46,11 +46,22 @@ import re
         inventory_txt = this will open a text file for us to export each output to.
                 
     Returns:
-        @BUG - https://github.com/calvinmorett/gearcost/issues/5
         find_sum: returns a 30 day average, or 0 if there is none sold within that time period. it does not work as intended when running the script 1/10/22. 
         y: ...
-    
 """
+###############################################################################
+"""
+    Changelog
+        1/10/22 - Removing wiki outputs.
+                - Changing order of functions below where it's blue vs green, the `Blue` server should be first.
+                - Adding reminders to places where terminal/cli gets an output/print, where I can add `yields`, that can be used on the flask-frontend.
+                - Adding reminders to remove `text` related items, for frontend (# can remove any txt related items, for frontend code)
+    Issues
+        1/10/22 - 30d average calculated, is not a 30d average.
+        https://github.com/calvinmorett/gearcost/issues/5      
+
+"""
+
 now = str(date.today())
 
 base = 'http://wiki.project1999.com/'
@@ -63,6 +74,7 @@ elif 'Green' in url:
     charname = url.replace('http://wiki.project1999.com/Magelo_Green:', '')
 elif 'Red' in url:
     print('Red is dead.')
+    # yield here...
 
 # code to set up initial scraping process  
 r = requests.get(url)
@@ -83,18 +95,17 @@ for link in connie:
         inventory.append(item_name)
 
 max_items = len(inventory)
-print('items = ',max_items)
+print('items = ',max_items) # yield here...
 cycle_item_count = 0
 
+# can remove any txt related items, for frontend code
 inventory_txt = open('inventory.txt', 'w+')
 
-# @BUG - Not working as intended. 30d average online displays 42, script displays 22. Not actually taking the 30d average, taking an all time average.
 def find_sum(str1):
     add_30day = sum(map(int,re.findall('\d+',str1)))
     avg = add_30day/2
     if find_sum == 0:
         print('None sold in the last 30 days')
-            
     return avg
 
 print(" ")
@@ -104,20 +115,14 @@ print(gearcost__header)
 inventory_txt.write(str(gearcost__header))
 
 def thegearcost():
-
-    ####################################
-    # Todo:
-    # calculate how many no drop items there are....
-    # calculate the prices for blue
-    # calculate the prices for green
-    ####################################
+    ###############################################################################
     # Ibol's Inventory Value:
     # --------------------------
     # Blue: 500p
     # Green: 800p
     # x5 NO DROP items
     # --------------------------
-    ####################################
+    ###############################################################################
     markdown_newline = '  '
     global char_header
     global gc_blue
@@ -125,28 +130,34 @@ def thegearcost():
     global no_drop_tally
     char_header = "\n" + charname+"`s Inventory Value\n--------------------------  "
     print(char_header)
-    inventory_txt.write(char_header)
+    # yield here...
+    inventory_txt.write(char_header) # can remove any txt related items, for frontend code
 
-    #global blue__gearcost $$
+    # print for debugging:
     #print(blue__gearcost)
     gc_blue = find_sum(str(blue__gearcost))
     print('Blue: ' + str(gc_blue), markdown_newline)
-    inventory_txt.write('\nBlue: ' + str(gc_blue) + '  ')
-
-    #global green__gearcost $$
+    # yield here...
+    inventory_txt.write('\nBlue: ' + str(gc_blue) + '  ') # can remove any txt related items, for frontend code
+    
+    # print for debugging:
     #print(green__gearcost)
     gc_green = find_sum(str(green__gearcost))
     print('Green: ' + str(gc_blue), markdown_newline)
-    inventory_txt.write('\nGreen: ' + str(gc_green) + '  ')
+    # yield here...
+    inventory_txt.write('\nGreen: ' + str(gc_green) + '  ') # can remove any txt related items, for frontend code
 
-    # no drop talley $$ no_drop_tally + NO DROP items
+    # counts the number of NO DROP
     no_drop_tally = len(no___drop)
     printed_nd_talley_terminal = "x" + str(no_drop_tally) + " NO DROP items\n--------------------------  "
     printed_nd_talley = "\nx" + str(no_drop_tally) + " NO DROP items\n--------------------------  "
     print(printed_nd_talley_terminal)
-    inventory_txt.write(str(printed_nd_talley))
+    # yield here...
+    inventory_txt.write(str(printed_nd_talley)) # can remove any txt related items, for frontend code
+   
 
-            ### commenting out wikicode
+###############################################################################
+# commenting out wikicode - 1/10/22
 ####################### wiki 
 # def wikicode():
 #     wiki_code = open('wikicode.txt', 'w+')
@@ -166,55 +177,60 @@ def thegearcost():
 #         wikicode()
 #     elif code_answer == 'no':
 #         print('See ya!')
-
-####################### wiki 
+###############################################################################
 
 no___drop = []
 blue__gearcost = []
 green__gearcost = []
 
-def get_item_name(slot_number):
-    #    global blue__gearcost
-    #    global green__gearcost
-    #    global no___drop
-    #    global no_drop_tally
-        
+def get_item_name(slot_number):     
     while slot_number <= max_items:
         if slot_number == max_items:
+            # run the function until the inventory doesn't contain anymore items
             thegearcost()
-            ### commenting out wikicode
+            ###############################################################################
+            # commenting out wikicode - 1/10/22
             # ask_wikicode()
             
         print(' ')
-        inventory_txt.write('\n')
+        # yield here...
+        inventory_txt.write('\n') # can remove any txt related items, for frontend code
         
         item_name = str(inventory[slot_number]).replace('http://wiki.project1999.com/', '')
         item_name = item_name.replace('_', ' ')
         print(item_name)
-        inventory_txt.write(str(item_name)+'\n')
+        inventory_txt.write(str(item_name)+'\n') # can remove any txt related items, for frontend code
         
         cycle_item_url = str(inventory[slot_number])
         r_itemurl = requests.get(cycle_item_url)
         soup_itemurl = BeautifulSoup(r_itemurl.text, 'html.parser')
 
         def get_item_price():
+            # these global functions handle the outputs you will see within the terminal and txt file
             global blue_nerd
-            global green_nerd
-            
-
-            def green_nerd():
-                green__report_terminal = '  Green 30-Day Average: ' + str(gmoney)
-                green__report = '  Green 30-Day Average: ' + str(gmoney) + '\n'
-                print(green__report_terminal)
-                inventory_txt.write(green__report)
-                green__gearcost.append(green__report)
+            global green_nerd        
                     
             def blue_nerd():
+                # intended for output within CLI terminal
                 blue__report_terminal = '  Blue 30-Day Average: ' + str(bmoney)
-                blue__report = '  Blue 30-Day Average: ' + str(bmoney) + '\n'
-                print(blue__report_terminal)
-                inventory_txt.write(blue__report)
-                blue__gearcost.append(blue__report)
+                print(blue__report_terminal)  
+                # yield here...
+                              
+                # intended for output within a text file
+                blue__report = '  Blue 30-Day Average: ' + str(bmoney) + '\n' # can remove any txt related items, for frontend code
+                inventory_txt.write(blue__report) # can remove any txt related items, for frontend code 
+                blue__gearcost.append(blue__report) # can remove any txt related items, for frontend code
+            
+            def green_nerd():
+                # intended for output within CLI terminal
+                green__report_terminal = '  Green 30-Day Average: ' + str(gmoney)
+                print(green__report_terminal)
+                # yield here...
+                
+                # intended for output within a text file
+                green__report = '  Green 30-Day Average: ' + str(gmoney) + '\n' # can remove any txt related items, for frontend code
+                inventory_txt.write(green__report) # can remove any txt related items, for frontend code
+                green__gearcost.append(green__report) # can remove any txt related items, for frontend code
                 
             if soup_itemurl.find("div", {"id": "auc_Blue"}) != None or soup_itemurl.find("div", {"id": "auc_Green"}) != None:
                 price_div = soup_itemurl.find("div", {"class": "auctrackerbox"})
@@ -225,6 +241,7 @@ def get_item_name(slot_number):
                     bmoney = blue_item.get_text().replace('±', '+')
                     bmoney = find_sum(bmoney)
                     
+                    # these global functions handle the outputs you will see within the terminal and txt file
                     blue_nerd()
                     
                     price_green = soup_itemurl.find("div", {"id": "auc_Green"})
@@ -232,13 +249,15 @@ def get_item_name(slot_number):
                     green_item = price_tab_green.find("td")
                     gmoney = green_item.get_text().replace('±', '+')
                     gmoney = find_sum(gmoney)
-                    
+
+                    # these global functions handle the outputs you will see within the terminal and txt file
                     green_nerd()
                     
                 if soup_itemurl.find("div", {"id": "auc_Blue"}) != None and soup_itemurl.find("div", {"id": "auc_Green"}) == None:
                     blue_no_green_prices = '  Prices found for Blue, but, No prices found for Green.'
                     print(blue_no_green_prices)
-                    inventory_txt.write(blue_no_green_prices)
+                    # yield here...
+                    inventory_txt.write(blue_no_green_prices) # can remove any txt related items, for frontend code
 
                     price_blue = soup_itemurl.find("div", {"id": "auc_Blue"})
                     price_tab_blue = price_blue.find("table", {"class": "eoTable3"})
@@ -246,11 +265,13 @@ def get_item_name(slot_number):
                     bmoney = blue_item.get_text().replace('±', '+')
                     bmoney = find_sum(bmoney)
                     
+                    # these global functions handle the outputs you will see within the terminal and txt file
                     blue_nerd()
                     
                 if soup_itemurl.find("div", {"id": "auc_Blue"}) == None and soup_itemurl.find("div", {"id": "auc_Green"}) != None:
                     green_no_blue_prices = '  Prices found for Green, but, No prices found for Blue.'
                     print(green_no_blue_prices)
+                    # yield here...
                     
                     price_green = soup_itemurl.find("div", {"id": "auc_Green"})
                     price_tab_green = price_green.find("table", {"class": "eoTable3"})
@@ -258,48 +279,60 @@ def get_item_name(slot_number):
                     gmoney = green_item.get_text().replace('±', '+')
                     gmoney = find_sum(gmoney)
                     
+                    # these global functions handle the outputs you will see within the terminal and txt file
                     green_nerd()
                     
-                    inventory_txt.write(green_no_blue_prices)
+                    inventory_txt.write(green_no_blue_prices) # can remove any txt related items, for frontend code
             else:
                 print('No Prices Found')
+                # yield here...
 
-        # mw-content-text is main item page you need to ask if had
-        # related uqest
-        # or item location displayed
+###############################################################################
+# Future Features
+        # # mw-content-text is main item page you need to ask if had
+        # # related quest
+        # # or item location displayed
         
-        item_page = soup_itemurl.find("div", {"id": "mw-content-text"})
-        item__page = item_page.text.strip()
+        # item_page = soup_itemurl.find("div", {"id": "mw-content-text"})
+        # item__page = item_page.text.strip(    
+
         
+        # # quest
+        # related_quest = soup_itemurl.find("span", {"id": "Related_quests"})
+        # related_quest__header = related_quest.text.strip()
+        
+        # #drops from
+        # drops_from = soup_itemurl.find("span", {"id": "Drops_From"})
+        # drops_from__header = related_quest.text.strip()
+        
+        # #Player_crafted
+        # player_crafted = soup_itemurl.find("span", {"id": "Player_crafted"})
+        # player_crafted__header = player_crafted.text.strip()
+        
+        # collect_links = item_page.findAll("a", {"class": 'mw-redirect'})
+        # collection_list = []
+        
+        # for link in collect_links:
+        #     if link['href']:
+        #         relative = link['href']
+        #         collection_list.append(urljoin(base, relative))
+        #         # print(collection_list)
+###############################################################################
+
         check_no_drop = soup_itemurl.find("div", {"class": "itemdata"})
         item_stats = check_no_drop.text.strip()
+        # debug print
         # print(item_stats)
         
-        # quest
-        related_quest = soup_itemurl.find("span", {"id": "Related_quests"})
-        related_quest__header = related_quest.text.strip()
-        
-        #drops from
-        drops_from = soup_itemurl.find("span", {"id": "Drops_From"})
-        drops_from__header = related_quest.text.strip()
-        
-        #Player_crafted
-        player_crafted = soup_itemurl.find("span", {"id": "Player_crafted"})
-        player_crafted__header = player_crafted.text.strip()
-        
-        collect_links = item_page.findAll("a", {"class": 'mw-redirect'})
-        collection_list = []
-        
-        for link in collect_links:
-            if link['href']:
-                relative = link['href']
-                collection_list.append(urljoin(base, relative))
-                # print(collection_list)
-                
         if 'DROP' in item_stats:
             no___drop.append('nodrop')
             print('  ' + 'NO DROP')
-            inventory_txt.write('  NO DROP'+'\n')  
+            # yield here...
+            inventory_txt.write('  NO DROP'+'\n') # can remove any txt related items, for frontend code
+
+
+###############################################################################
+# Future Features
             # print('Find this items Quest or Location')
 #            if 'Quests' in item__page:
 #                print(related_quest__header,collection_list)
@@ -310,8 +343,10 @@ def get_item_name(slot_number):
 #            if 'Player Crafted' in item__page:
 #                print(player_crafted__header,collection_list)
 #                slot_number += 1
-                
+###############################################################################  
+
             slot_number += 1
+            # debug print
             # print(item_name)
             get_item_name(slot_number)
         else:
@@ -326,4 +361,3 @@ try:
     get_item_name(cycle_item_count)
 except IndexError:
     pass
-
